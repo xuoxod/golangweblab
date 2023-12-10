6e723556bd69a16ff8cec31e2cd4a1cbecd5042b
rick xuoxod <xuoxod@gmail.com>
2023-12-10 07:08:50 -0500

    Deleted old code

--- pkg/utils/jwtvalidator.go
+++ pkg/utils/jwtvalidator.go
@@ -8,32 +8,6 @@ import (
 	"github.com/xuoxod/weblab/pkg/constants"
 )
 
-/* func ValidateTokenfunc(tokenString string) (bool, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return false, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return constants.SecretKey, nil
-	})
-
-	if err != nil {
-		fmt.Println(err)
-		return false, err
-
-	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		fmt.Println(claims["user"], claims["nbf"])
-		return true, nil
-	} else {
-		fmt.Println(err)
-		return false, err
-	}
-} */
-
 func ValidateToken(cookie *http.Cookie) (*jwt.Token, bool, error) {
 	token, err := jwt.ParseWithClaims(cookie.Value, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(constants.SecretKey), nil
