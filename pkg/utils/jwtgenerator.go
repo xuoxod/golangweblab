af148ce09743c1279ff2e6bde92d70a5c54f5831
rick xuoxod <xuoxod@gmail.com>
2023-12-10 07:07:58 -0500

    Deleted old code

--- pkg/utils/jwtgenerator.go
+++ pkg/utils/jwtgenerator.go
@@ -9,23 +9,6 @@ import (
 	"github.com/xuoxod/weblab/pkg/constants"
 )
 
-/* func GenerateJWT(id uint64) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-
-	claims["authorized"] = true
-	claims["user_id"] = id
-	claims["exp"] = time.Now().Add(time.Second * 23).Unix()
-
-	tokenString, err := token.SignedString(constants.SecretKey)
-
-	if err != nil {
-		_ = fmt.Errorf("Something Went Wrong: %s", err.Error())
-		return "", err
-	}
-	return tokenString, nil
-} */
-
 func GenerateJwt(id int) (string, error) {
 	strId := strconv.Itoa(id)
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, &jwt.StandardClaims{
