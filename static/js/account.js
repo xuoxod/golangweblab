b6367b886a812630d9c1777ace3d4a971deb344a
rick xuoxod <xuoxod@gmail.com>
2024-01-24 07:09:02 -0500

    Modified account script

--- static/js/account.js
+++ static/js/account.js
@@ -1,1 +1,18 @@
-log(`Account View\n\n`);
+document.addEventListener("DOMContentLoaded", () => {
+  // Controls
+  const accountPicture = document.querySelector("#account-picture");
+  const uploadPictureButton = document.querySelector("#upload-picture-button");
+  const changePasswordParent = document.querySelector(
+    "#change-password-parent"
+  );
+  const changePasswordDiv = document.querySelector("#change-password-div");
+  const changePasswordButton = document.querySelector(
+    "#change-password-button"
+  );
+  const changeEmailButton = document.querySelector("#change-email-button");
+
+  // Values
+  const password = document.querySelector("#password").value;
+  const email = document.querySelector("#email").value;
+  const phone = document.querySelector("#phone").value;
+});
