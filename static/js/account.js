8b3c9e15dc9c97c1de9677147762b73e9e72ac68
rick xuoxod <xuoxod@gmail.com>
2024-01-28 05:04:02 -0500

    Added comments

--- static/js/account.js
+++ static/js/account.js
@@ -1,22 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Controls
+  /* Controls */
+
+  // Account Picture
   const accountPicture = document.querySelector("#account-picture");
   const uploadPictureButton = document.querySelector("#upload-picture-button");
   const changePasswordParent = document.querySelector(
     "#change-password-parent"
   );
+
+  // Change Password
   const changePasswordDiv = document.querySelector("#change-password-div");
   const changePasswordButton = document.querySelector(
     "#change-password-button"
   );
+
+  // Change Email
   const changeEmailParent = document.querySelector("#change-email-parent");
   const changeEmailDiv = document.querySelector("#change-email-div");
   const changeEmailButton = document.querySelector("#change-email-button");
+
+  // Change Phone
   const changePhoneParent = document.querySelector("#change-phone-parent");
   const changePhoneDiv = document.querySelector("#change-phone-div");
   const changePhoneButton = document.querySelector("#change-phone-button");
 
-  // Values
+  // Input Values
   const password = document.querySelector("#password").value;
   const email = document.querySelector("#email").value;
   const phone = document.querySelector("#phone").value;
