87abde5e387181d7d3a6e6e5412ec49cfd81e812
rick xuoxod <xuoxod@gmail.com>
2024-06-08 05:02:52 -0400

    Modified constants

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -3,43 +3,43 @@ package constants
 var SecretKey []byte = []byte("JELsI,bRyEgIKlRU%")
 
 // Date
-var StrLongDateOnly = "January 2, 2006"
-var StrShortDateOnly = "Jan 2, 2006"
-var NumLongDateOnly = "01/02/2006"
-var NumShortDateOnly = "1/2/06"
+const StrLongDateOnly = "January 2, 2006"
+const StrShortDateOnly = "Jan 2, 2006"
+const NumLongDateOnly = "01/02/2006"
+const NumShortDateOnly = "1/2/06"
 
-// var StrLongDateOnly = time.Now().Format(strLongDateOnly)
-// var StrShortDateOnly = time.Now().Format(strShortDateOnly)
-// var NumLongNumberDateOnly = time.Now().Format(numLongDateOnly)
-// var NumShortNumberDateOnly = time.Now().Format(numShortDateOnly)
+// const StrLongDateOnly = time.Now().Format(strLongDateOnly)
+// const StrShortDateOnly = time.Now().Format(strShortDateOnly)
+// const NumLongNumberDateOnly = time.Now().Format(numLongDateOnly)
+// const NumShortNumberDateOnly = time.Now().Format(numShortDateOnly)
 
 // Time
-var NumNormalTime = "3:04pm"
-var NumNormalTimeFull = "3:04:05pm"
-var NumMilitaryTime = "15:04"
-var NumMilitaryTimeFull = "15:04:05"
+const NumNormalTime = "3:04pm"
+const NumNormalTimeFull = "3:04:05pm"
+const NumMilitaryTime = "15:04"
+const NumMilitaryTimeFull = "15:04:05"
 
-// var NumNormalTime = time.Now().Format(numNormalTime)
-// var NumNormalTimeFull = time.Now().Format(numNormalTimeFull)
-// var NumMilitaryTime = time.Now().Format(numMilitaryTime)
-// var NumMilitaryTimeFull = time.Now().Format(numMilitaryTimeFull)
+// const NumNormalTime = time.Now().Format(numNormalTime)
+// const NumNormalTimeFull = time.Now().Format(numNormalTimeFull)
+// const NumMilitaryTime = time.Now().Format(numMilitaryTime)
+// const NumMilitaryTimeFull = time.Now().Format(numMilitaryTimeFull)
 
 // Date and Time
-var StrLongDateNormalTime = "January 2, 2006,3:04pm"
-var StrLongDateNormalTimeFull = "January 2, 2006,3:04:05pm"
-var StrShortDateNormalTime = "Jan 2,2006,3:04pm"
-var StrShortDateNormalTimeFull = "Jan 2,2006,3:04:05pm"
-var StrLongDateMilitaryTime = "January 2, 2006,15:04"
-var StrLongDateMilitaryTimeFull = "January 2, 2006,15:04:05"
-var NumLongDateNormalTime = "01/02/2006,3:04"
-var NumLongDateNormalTimeFull = "01/02/2006,3:04:05pm"
-var NumLongDateMilitaryTime = "01/02/2006,15:04:05"
+const StrLongDateNormalTime = "January 2, 2006,3:04pm"
+const StrLongDateNormalTimeFull = "January 2, 2006,3:04:05pm"
+const StrShortDateNormalTime = "Jan 2,2006,3:04pm"
+const StrShortDateNormalTimeFull = "Jan 2,2006,3:04:05pm"
+const StrLongDateMilitaryTime = "January 2, 2006,15:04"
+const StrLongDateMilitaryTimeFull = "January 2, 2006,15:04:05"
+const NumLongDateNormalTime = "01/02/2006,3:04"
+const NumLongDateNormalTimeFull = "01/02/2006,3:04:05pm"
+const NumLongDateMilitaryTime = "01/02/2006,15:04:05"
 
-// var StrLongDateNormalTime = time.Now().Format(strLongDateNormalTime)
-// var StrShortDateNormalTime = time.Now().Format(strShortDateNormalTime)
-// var StrLongDateNormalTimeFull = time.Now().Format(strLongDateNormalTimeFull)
-// var StrShortDateNormalTimeFull = time.Now().Format(strShortDateNormalTimeFull)
-// var StrLongDateMilitaryTime = time.Now().Format(strLongDateMilitaryTime)
-// var StrLongDateMilitaryTimeFull = time.Now().Format(strLongDateMilitaryTimeFull)
-// var NumLongDateNormalTime = time.Now().Format(numLongDateNormalTime)
-// var NumLongDateNormalTimeFull = time.Now().Format(numLongDateNormalTimeFull)
+// const StrLongDateNormalTime = time.Now().Format(strLongDateNormalTime)
+// const StrShortDateNormalTime = time.Now().Format(strShortDateNormalTime)
+// const StrLongDateNormalTimeFull = time.Now().Format(strLongDateNormalTimeFull)
+// const StrShortDateNormalTimeFull = time.Now().Format(strShortDateNormalTimeFull)
+// const StrLongDateMilitaryTime = time.Now().Format(strLongDateMilitaryTime)
+// const StrLongDateMilitaryTimeFull = time.Now().Format(strLongDateMilitaryTimeFull)
+// const NumLongDateNormalTime = time.Now().Format(numLongDateNormalTime)
+// const NumLongDateNormalTimeFull = time.Now().Format(numLongDateNormalTimeFull)
